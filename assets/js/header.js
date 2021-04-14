52caa95ee8967d0457f039c820f4b973e12213bd
matheus souza <ruivorobado@gmail.com>
2021-04-14 18:31:02 -0300

    menu mobile corrijido

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,27 @@
+/*
 function abreMenu() {
-    let menu = document.querySelector('nav.menu-mobile').style
-    if (menu.display == 'block') {
-        menu.display = 'none'
+    let menu = document.querySelector('nav.menu-mobile')
+    if (menu.classList.contains('aparece')) {
+        menu.classList.add('some')
+        menu.classList.remove('aparece')
     } else {
-        menu.display = 'block'
-
+        menu.classList.add('aparece')
+        menu.classList.remove('some')
     }
 }
+*/
+const menu = document.querySelector('nav.menu-mobile')
+
+document.addEventListener('click', e => {
+    el = e.target
+    if(el.classList.contains('btn-menu-mobile')){
+        if(menu.classList.contains('aparece')){
+            menu.classList.add('some')
+            menu.classList.remove('aparece')
+            return
+        }
+        menu.classList.add('aparece')
+        menu.classList.remove('some')
+    return
+    }
+})
