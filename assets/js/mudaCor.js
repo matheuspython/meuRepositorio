8d979ce928a4a4d161bbd6d2f507ab606ba1d02b
matheus souza <ruivorobado@gmail.com>
2021-04-14 21:28:33 -0300

    refatorando codigo de mudança no tema

--- assets/js/mudaCor.js
+++ assets/js/mudaCor.js
@@ -1,36 +1,25 @@
-const muda = document.querySelector('.luz')
 let teste;
 
+function molde(elemento, cor, outraCor){
+  return  {
+    elemento,
+    cor,
+    outraCor,
+    
+    muda(teste){
+      if(teste == true){
+        mudaCor(this.elemento, this.outraCor)
+        return
+      }
+        mudaCor(this.elemento, this.cor)
+    }
+  }
+}
+
+
 const elementos = [
-  {
-    query:document.querySelector('header.header'),
-    cor:'rgb(67, 80, 88)',
-    outraCor:'rgb(141, 182, 207)',
-    
-    muda(teste){
-      if(teste == true){
-        mudaCor(this.query, this.outraCor)
-      }else{
-        mudaCor(this.query, this.cor)
-      }
-
-    }
-  },
-  {
-    query:document.querySelector('section.topo'),
-    cor:'rgb(22, 21, 21)',
-    outraCor:'rgb(1, 90, 97)',
-    
-    muda(teste){
-      if(teste == true){
-        mudaCor(this.query, this.outraCor)
-      }else{
-        mudaCor(this.query, this.cor)
-      }
-
-    }
-  },
-
+  molde(document.querySelector('header.header'),'rgb(67, 80, 88)','rgb(141, 182, 207)'),
+  molde(document.querySelector('section.topo'),'rgb(22, 21, 21)','rgb(1, 90, 97)'),
 ]
 
 function mudaCor(element, cor) {
@@ -41,19 +30,14 @@ function mudaCor(element, cor) {
 
 document.addEventListener('click', e => {
   el = e.target
-  if(el.classList.contains('luz')){
+  if(e.target.classList.contains('luz')){
     if(teste == true){
       teste = false
-      elementos.forEach(value => {
-        value.muda(teste)
-       
-      })
+      elementos.forEach(value => value.muda(teste))
       return
     }
     teste = true
-    elementos.forEach(value =>{
-      value.muda(teste)
-    })
+    elementos.forEach(value => value.muda(teste))
   }
 })
 
