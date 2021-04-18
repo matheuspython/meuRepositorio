5aedec9e6006237f81fa1aa7fe664d52428d92fe
matheus souza <ruivorobado@gmail.com>
2021-04-17 21:23:20 -0300

    modo green completo

--- assets/js/mudaCor.js
+++ assets/js/mudaCor.js
@@ -20,6 +20,10 @@ function molde(elemento, cor, outraCor){
 const elementos = [
   molde(document.querySelector('header.header'),'rgb(67, 80, 88)','rgb(141, 182, 207)'),
   molde(document.querySelector('section.topo'),'rgb(22, 21, 21)','rgb(1, 90, 97)'),
+  molde(document.querySelector('section.tecnologias'),'rgb(30, 30, 30)','rgb(5, 168, 170)'),
+  molde(document.querySelector('section.projetos'),'rgb(30, 30, 30)','rgb(2, 52, 54)'),
+  molde(document.querySelector('footer.footer'),'rgb(12, 12, 12)','rgb(141, 182, 207)')
+
 ]
 
 function mudaCor(element, cor) {
